6e4731fcadaee91be3aefb81a1688bc0e43150b2
abhishekkushwaha4u <abhishek.kushwaha2018@vitstudent.ac.in>
2020-05-20 06:51:14 +0530

    modified url structure for retrieve, update delete

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('script/', views.ScriptListCreateAPIView.as_view()),
-    path('script/{}/', views.ScriptRetrieveUpdateDestroyAPIView.as_view()),
+    path('script/<pk>/', views.ScriptRetrieveUpdateDestroyAPIView.as_view()),
 ]
