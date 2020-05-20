799dc7eabfc612f07c88a7ed9a863034d237fd55
abhishekkushwaha4u <abhishek.kushwaha2018@vitstudent.ac.in>
2020-05-20 06:44:11 +0530

    modified main views for crud

--- main/views.py
+++ main/views.py
@@ -1,15 +1,15 @@
 from rest_framework import generics
+from rest_framework.pagination import LimitOffsetPagination
 from .models import Script
 from .serializers import ScriptSerializer
 
 class ScriptListCreateAPIView(generics.ListCreateAPIView):
     permission_classes = []
-    pagination_class = []
+    pagination_class = LimitOffsetPagination
     queryset = Script.objects.all()
     serializer_class = ScriptSerializer
 
 class ScriptRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = []
-    pagination_class = []
     queryset = Script.objects.all()
     serializer_class = ScriptSerializer
