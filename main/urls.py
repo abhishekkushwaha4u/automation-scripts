5762f111438424bcee0bb4313edbbe7b7de67d08
abhishekkushwaha4u <abhishek.kushwaha2018@vitstudent.ac.in>
2020-05-28 23:22:38 +0530

    added routes for adding retrieving scripts by name and number

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('script/', views.ScriptListCreateAPIView.as_view()),
     path('script/<pk>/', views.ScriptRetrieveUpdateDestroyAPIView.as_view()),
+    path('search/script/byName/', views.ScriptRetrieveByNameView.as_view()),
+    path('search/script/byNumber/', views.ScriptRetrieveByNoOfRecords.as_view()),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.pagination import LimitOffsetPagination
 from .models import Script
 from .serializers import ScriptSerializer
@@ -13,3 +15,26 @@ class ScriptRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = []
     queryset = Script.objects.all()
     serializer_class = ScriptSerializer
+
+
+class ScriptRetrieveByNameView(APIView):
+    def get(self, request):
+        name = request.query_params.get('name')
+        if name:
+            queryset = Script.objects.filter(name__icontains=name)
+        else:
+            queryset = Script.objects.all()
+        serializer = ScriptSerializer(queryset, many=True)
+        return Response(serializer)
+
+class ScriptRetrieveByNoOfRecords(APIView):
+
+    def get(self, request):
+        offset = int(request.query_params.get('offset'))
+        limit = int(request.query_params.get('limit'))
+        if offset and limit:
+            queryset = Script.objects.all()[offset: offset+limit]
+        else:
+            queryset = Script.objects.all()
+        serializer = ScriptSerializer(queryset, many=True)
+        return Response(serializer)
